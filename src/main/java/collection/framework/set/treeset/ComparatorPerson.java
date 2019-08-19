51c74ba4de37c77dc06cdd2cdc0dd551282ddb7e
idionkim <ksundong@gmail.com>
2019-08-19 23:21:34 +0900

    Rename Duplicate Class

--- src/main/java/collection/framework/set/treeset/ComparatorPerson.java
+++ src/main/java/collection/framework/set/treeset/ComparatorPerson.java
@@ -3,11 +3,11 @@ package collection.framework.set.treeset;
 import java.util.Comparator;
 import java.util.TreeSet;
 
-class Person implements Comparable<Person> {
+class PersonTwo implements Comparable<PersonTwo> {
 	String name;
 	int age;
 	
-	public Person(String name, int age) {
+	public PersonTwo(String name, int age) {
 		this.name = name;
 		this.age = age;
 	}
@@ -16,25 +16,25 @@ class Person implements Comparable<Person> {
 	public String toString() { return name + " : " + age; }
 	
 	@Override
-	public int compareTo(Person p) {
+	public int compareTo(PersonTwo p) {
 		return this.age - p.age;
 	}
 }
 
-class PersonComparator implements Comparator<Person> {
-	public int compare(Person p1, Person p2) {
+class PersonComparator implements Comparator<PersonTwo> {
+	public int compare(PersonTwo p1, PersonTwo p2) {
 		return p2.age - p1.age;
 	}
 }
 
 public class ComparatorPerson {
 	public static void main(String[] args) {
-		TreeSet<Person> tree = new TreeSet<>(new PersonComparator());
-		tree.add(new Person("KIM", 25));
-		tree.add(new Person("PARK", 39));
-		tree.add(new Person("KONG", 37));
+		TreeSet<PersonTwo> tree = new TreeSet<>(new PersonComparator());
+		tree.add(new PersonTwo("KIM", 25));
+		tree.add(new PersonTwo("PARK", 39));
+		tree.add(new PersonTwo("KONG", 37));
 		
-		for (Person p : tree) {
+		for (PersonTwo p : tree) {
 			System.out.println(p);
 		}
 	}
