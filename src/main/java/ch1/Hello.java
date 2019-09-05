b74b750fa991d5d03164c382c3d1f1cbdf01ecef
idionKim <ksundong@gmail.com>
2019-09-06 06:00:00 +0900

    Add comment in Hello.java

--- src/main/java/ch1/Hello.java
+++ src/main/java/ch1/Hello.java
@@ -1,7 +1,13 @@
 package ch1;
 
+/*
+Date    : 2019. 9. 6
+Source  : Hello.java
+Author  : IdionKim
+Email   : ksundong@gmail.com
+ */
 public class Hello {
-	public static void main(String[] args) {
+	public static void main(String[] args) {    /* Program Start */
 		System.out.println("Hello, world."); // Print Hello, world.
 	}
 }
